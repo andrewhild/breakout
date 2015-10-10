6f96890bd918148258e72c0c8edbca10cb748ab7
Andrew Hild <republiqueajamais@hotmail.com>
2015-10-10 16:28:12 -0700

    Added class definitions and some boilerplate in main

---
+++ src/Breakout.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Breakout extends JFrame {
+
+	/**
+	 * @param args 
+	 * 		might be used later
+	 */
+	public static void main(String[] args) {
+		init();
+	}
+	
+	/**
+	 * Initializes the field object and displays the game
+	 */
+	private static void init(){
+		JFrame game = new Breakout();
+		JPanel field = new Field();
+		game.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		game.add(field);
+		game.pack();
+		game.setVisible(true);
+	}
+
+}
