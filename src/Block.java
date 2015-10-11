08232faf6219ae8073b3c1c9387e7fbf9b4f86f7
Andrew Hild <republiqueajamais@hotmail.com>
2015-10-10 17:26:48 -0700

    Added basic instance variable and method structure to most classes.

--- src/Block.java
+++ src/Block.java
@@ -1,6 +1,50 @@
+import java.awt.Color;
+import java.awt.Rectangle;
+
 
 public class Block {
 
+	private int x, y, len, wid;
+	private Color color;
 	
-
+	/**
+	 * 
+	 * @param px
+	 * 	x-coordinate of center
+	 * @param py
+	 * 	y-coordinate of center
+	 * @param l
+	 * 	length of block
+	 * @param w
+	 * 	width of block
+	 * @param c
+	 * 	color of block
+	 */
+	public Block(int px, int py, int l, int w, Color c){
+		x = px;
+		y = py;
+		len = l;
+		wid = w;
+		color = c;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	/**
+	 * Create a rectangle representation of a block. Note that rectangle length and width should almost
+	 * certainly be an even number so that this doesn't break
+	 * @return
+	 * 	A Rectangle representing the block
+	 */
+	public Rectangle getRect(){
+		return new Rectangle(x-len/2, y-wid/2, len, wid);
+	}
+	
+	
 }
--- src/Field.java
+++ src/Field.java
@@ -1,6 +1,60 @@
 import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.HashMap;
 
 
-public class Field extends JPanel {
+public class Field extends JPanel implements ActionListener, KeyListener{
+	
+	private static int frameRate = 60;
+	private HashMap<Point,Block> blocks;
+	private Paddle paddle;
+	private Ball b;
+	
+	/**
+	 * The big cookie that does most everything
+	 */
+	private void update(){
+		
+	}
+	
+	/**
+	 * Paints the updated game state, called every 1/frameRate seconds
+	 * @param g
+	 * 	The graphics context used to paint
+	 */
+	public void paint(Graphics g){
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//Hooray and taz'dingo for vestigial methods
+	
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
