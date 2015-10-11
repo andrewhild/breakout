7e8f5e3d2475d7c74bfe772f346d1b7ec958c1f6
Andrew Hild <republiqueajamais@hotmail.com>
2015-10-10 17:56:34 -0700

    added physicsMan class. Will try to implement later if it seems useful.

---
+++ src/physicsMan.java
@@ -0,0 +1,19 @@
+
+public class physicsMan {
+
+	private float vx, vy;
+	private boolean fixed, moveable, deformable;
+	private int px, py;
+	
+	public physicsMan(){
+		
+	}
+	
+	public void updatePos(){
+		
+	}
+	
+	public void resolveCollision(){
+		
+	}
+}
